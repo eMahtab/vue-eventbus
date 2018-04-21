a94a86d4bf281f9a7d5739c8bbb68bde9b7506ee
Mahtab Alam <alammahtab08@gmail.com>
2018-04-21 10:28:00 +0530

    Example showing how to use EventBus for communication between sibling components

---
+++ src/components/Child1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="child1" class="component">
+    
+    <h2>Child 1 Component</h2>
+    <span>{{message}}</span>
+    
+  </div>
+</template>
+
+<script>
+import {EventBus} from '../main.js'
+
+export default {
+  props: ['message'],
+  created(){
+    EventBus.$on('messageUpdate',(newMessage) => {
+        this.message = newMessage
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#child1 {
+  background-color: lightseagreen;
+  
+}
+</style>
---
+++ src/components/Child2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="child2" class="component">
+    
+    <h2>Child 2 Component</h2>
+    <span>{{message}}</span>
+    <br/><br/>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+
+<script>
+import {EventBus} from '../main.js'
+
+export default {
+  props:['message'],
+  methods:{
+    changeMessage(){
+      this.message = "A day of worry is more exhausting than a week of work - John Lubbock"
+      EventBus.updateMessage(this.message)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#child2 {
+  background-color: lightcoral;
+}
+
+</style>
